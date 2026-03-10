b0449a058645dd3aeb6bedf6188c1ad8dcce80a5
Kishore <22211A1121@bvrit.ac.in>
2026-03-10 13:55:52 +0530

    Fix undefined kIsWeb error in LoginScreen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' as sb;
